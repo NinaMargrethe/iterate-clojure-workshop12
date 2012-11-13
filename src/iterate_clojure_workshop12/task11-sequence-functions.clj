29446f97a12232855146215893a952198e54c6fa
Lars Barlindhaug <lars.barlindhaug@iterate.no>
2012-11-13 19:13:30 +0100

    solutions/task mixup

--- src/iterate_clojure_workshop12/task11-sequence-functions.clj
+++ src/iterate_clojure_workshop12/task11-sequence-functions.clj
@@ -1,31 +1,27 @@
 (ns iterate-clojure-workshop12.task11-map)
 
 ;; map
-;; Use the * function to sum to lists '(1 2 3) and '(2 2 2) to '(2 4 6)
+;; Use the + function to sum to lists '(1 2 3) and '(2 2 2) to '(3 4 5)
 
-(map * '(1 2 3) '(2 2 2))
-
+; TODO
 
 ;; reduce
 ;; Solve 1 + 2 + 3 using reduce and not (+ 1 2 3).
 
-(reduce + '(1 2 3))
+; TODO
 
 
 ;; apply
 ;; Find the max value from the list '(2 6 4)
 
-(apply max '(2 6 4))
-
+; TODO
 
 ;; Use map and apply to find how old the oldest person in the list is in this map:
 ;; '({ :name "Lars", :age 25 }, { :name "Jakub", :age 32 })
 
-(apply max 
-  (map :age 
-    '({ :name "Lars", :age 25 }, 
-      { :name "Jakub", :age 32 })))
+; TODO
 
 ;; Modify the previous solution to find the complete map of the oldest person using reduce.
 ;; TIP: use the second form of reduce with an initial value.
 
+; TODO
--- src/iterate_clojure_workshop12/task11-sequence-functions.solutions.clj
+++ src/iterate_clojure_workshop12/task11-sequence-functions.solutions.clj
@@ -1,27 +1,30 @@
 (ns iterate-clojure-workshop12.task11-map)
 
 ;; map
-;; Use the + function to sum to lists '(1 2 3) and '(2 2 2) to '(3 4 5)
+;; Use the * function to sum to lists '(1 2 3) and '(2 2 2) to '(2 4 6)
 
-; TODO
+(map * '(1 2 3) '(2 2 2))
+
 
 ;; reduce
 ;; Solve 1 + 2 + 3 using reduce and not (+ 1 2 3).
 
-; TODO
+(reduce + '(1 2 3))
 
 
 ;; apply
 ;; Find the max value from the list '(2 6 4)
 
-; TODO
+(apply max '(2 6 4))
+
 
 ;; Use map and apply to find how old the oldest person in the list is in this map:
 ;; '({ :name "Lars", :age 25 }, { :name "Jakub", :age 32 })
 
-; TODO
+(apply max
+  (map :age
+    '({ :name "Lars", :age 25 }, 
+      { :name "Jakub", :age 32 })))
 
 ;; Modify the previous solution to find the complete map of the oldest person using reduce.
 ;; TIP: use the second form of reduce with an initial value.
-
-; TODO
