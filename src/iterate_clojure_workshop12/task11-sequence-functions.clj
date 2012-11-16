e2e461e014b7e005ea0566c579077c90ad69bb26
Jakub Holy <jakub.holy@iterate.no>
2012-11-16 14:27:09 +0100

    Small improvements task11

--- src/iterate_clojure_workshop12/task11-sequence-functions.clj
+++ src/iterate_clojure_workshop12/task11-sequence-functions.clj
@@ -1,7 +1,7 @@
 (ns iterate-clojure-workshop12.task11-map)
 
 ;; map
-;; Use the + function to sum to lists '(1 2 3) and '(2 2 2) to '(3 4 5)
+;; Use the + function to sum two lists '(1 2 3) and '(2 2 2) to (the expected result being '(3 4 5))
 
 ; TODO
 
@@ -12,7 +12,8 @@
 
 
 ;; apply
-;; Find the max value from the list '(2 6 4)
+;; You can find out the maximum of numbers using (max 2 6 4)
+;; but here you got the collection '(2 6 4) containing the numbers
 
 ; TODO
 
@@ -22,6 +23,5 @@
 ; TODO
 
 ;; Modify the previous solution to find the complete map of the oldest person using reduce.
-;; TIP: use the second form of reduce with an initial value.
 
 ; TODO
